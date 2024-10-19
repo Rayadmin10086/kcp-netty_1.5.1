7f53cf55c8780a0d7024bc1762c76ad91dc22d2d
Rayadmin10086 <137618375+Rayadmin10086@users.noreply.github.com>
2024-10-19 15:23:40 +0800

    Create KcpServer$1.java

---
+++ kcp-netty/src/main/java/io/jpower/kcp/netty/KcpServer$1.java
@@ -0,0 +1,29 @@
+package io.jpower.kcp.netty;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+
+class KcpServer$1 extends ChannelInitializer<Channel> {
+   KcpServer$1(KcpServer this$0, ChannelConfig var2, KcpListener var3) {
+      this.this$0 = this$0;
+      this.val$channelConfig = var2;
+      this.val$kcpListener = var3;
+   }
+
+   protected void initChannel(Channel ch) {
+      ServerChannelHandler serverChannelHandler = new ServerChannelHandler(
+         this.this$0.channelManager, this.val$channelConfig, this.this$0.iMessageExecutorPool, this.val$kcpListener, this.this$0.hashedWheelTimer
+      );
+      ChannelPipeline cp = ch.pipeline();
+      if (this.val$channelConfig.isCrc32Check()) {
+         Crc32Encode crc32Encode = new Crc32Encode();
+         Crc32Decode crc32Decode = new Crc32Decode();
+         cp.addLast(new ChannelHandler[]{crc32Encode});
+         cp.addLast(new ChannelHandler[]{crc32Decode});
+      }
+
+      cp.addLast(new ChannelHandler[]{serverChannelHandler});
+   }
+}
